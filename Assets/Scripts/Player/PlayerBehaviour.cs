1669693d343cfd77073eb7777074a9badbd6b96e
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2016-10-11 16:42:04 -0400

    Added new player animations. Animations are polished. Need review on playerBehaviour scripts

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -21,7 +21,7 @@ public class PlayerBehaviour : MonoBehaviour {
     // Change to proper ground layer number if needed
     public LayerMask groundLayer;
     //Info vars
-    public bool isFlying, onGround;
+    public bool isFlying, onGround, facingRight;
 	//Anim Tree
 	private Animator playerAnim;
 
@@ -42,19 +42,32 @@ public class PlayerBehaviour : MonoBehaviour {
     // Update is used for receiving input
     void Update () {
         direction = Input.GetAxisRaw("Horizontal");
-        if (!verticalCD && Input.GetAxisRaw("Vertical") > 0)
+		lift = Input.GetButtonDown("Vertical")? 1.0f:0.0f;
+		/*if (!verticalCD && Input.GetAxisRaw("Vertical") > 0)
         {
             lift = Input.GetAxisRaw("Vertical");
         }
         else {
             lift = 0.0f;
         }    
-        verticalCD = Input.GetAxisRaw("Vertical") != 0.0f;
+        verticalCD = Input.GetAxisRaw("Vertical") != 0.0f;*/
+		isFlying = lift != 0 && !onGround;
+		onGround = isGrounded();
+		Vector2 playerMove = new Vector2(direction * horizontalSpd, lift * verticalSpd);
+		if (lift != 0 || onGround)
+			playerRB.AddForce(playerMove, ForceMode2D.Impulse);
+		if (Mathf.Abs(playerRB.velocity.x) > maxHorizontalSpd)
+		{
+			playerRB.velocity = new Vector2(Mathf.Sign(playerRB.velocity.x)*maxHorizontalSpd, playerRB.velocity.y);
+		}
+		isFlying = lift != 0 && !onGround;
+		onGround = isGrounded();
+		setPlayerAnimVars();
     }
 
     // FixedUpdate is used for movement and methods involving rigidbody
     void FixedUpdate() {
-        isFlying = lift != 0 && !onGround;
+    	/*isFlying = lift != 0 && !onGround;
         onGround = isGrounded();
         Vector2 playerMove = new Vector2(direction * horizontalSpd, lift * verticalSpd);
         if (lift != 0 || onGround)
@@ -65,13 +78,31 @@ public class PlayerBehaviour : MonoBehaviour {
         }
         isFlying = lift != 0 && !onGround;
         onGround = isGrounded();
-		setPlayerAnimVars();
+		setPlayerAnimVars();*/
+
+		//Facing Directions
+		if (direction < 0 && facingRight) {	//Moving Left, facing right (flip)
+			Flip ();
+		} 
+		else if (direction > 0 && !facingRight) { //Moving Right, facing left (flip)
+			Flip ();	
+		}
     }
 
     bool isGrounded()
     {
         return Physics2D.Raycast(transform.position, -Vector2.up, distance:distToGround, layerMask:groundLayer.value);
     }
+
+	void Flip(){
+		//Flip the character Model
+		facingRight = !facingRight;
+
+		//Set the new localScale to be inverted
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 
 	void setPlayerAnimVars(){
 		playerAnim.SetBool ("grounded", onGround);
