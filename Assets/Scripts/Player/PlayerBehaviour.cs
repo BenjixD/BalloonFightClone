784dc91d84c05b0383ac6002f5f27e430facffd1
nansonzheng <nanson.zheng@mail.utoronto.ca>
2016-10-11 03:23:54 -0400

    Added onGround and isFlying info vars, changed rigidbody parameters to be set from script

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -5,23 +5,39 @@ public class PlayerBehaviour : MonoBehaviour {
 
     Rigidbody2D playerRB;
     float direction, lift;
+    float distToGround;
+
+    // Speed adjustment vars
     public float horizontalSpd = 0.4f;
-    public float verticalSpd = 2.0f;
+    public float verticalSpd = 2.8f;
     public float maxHorizontalSpd = 6.0f;
+    public float gravityScale = 0.6f;
+    public float linearDrag = 1.0f;
+    
 
     //Simulate Button spamming
     bool verticalCD;
 
+    // Change to proper ground layer number if needed
+    public LayerMask groundLayer;
+    //Info vars
+    public bool isFlying, onGround;
+
 	// Use this for initialization
 	void Start () {
         playerRB = GetComponent<Rigidbody2D>();
+        playerRB.gravityScale = gravityScale;
+        playerRB.drag = linearDrag;
         direction = 0.0f;
         lift = 0.0f;
         verticalCD = false;
+        distToGround = GetComponent<Collider2D>().bounds.extents.y * 1.2f;
+        isFlying = onGround = false;
+        groundLayer = LayerMask.GetMask("Ground");
     }
-	
-	// Update is used for receiving input
-	void Update () {
+
+    // Update is used for receiving input
+    void Update () {
         direction = Input.GetAxisRaw("Horizontal");
         if (!verticalCD && Input.GetAxisRaw("Vertical") > 0)
         {
@@ -31,7 +47,6 @@ public class PlayerBehaviour : MonoBehaviour {
             lift = 0.0f;
         }    
         verticalCD = Input.GetAxisRaw("Vertical") != 0.0f;
-
     }
 
     // FixedUpdate is used for movement and methods involving rigidbody
@@ -42,5 +57,12 @@ public class PlayerBehaviour : MonoBehaviour {
         {
             playerRB.velocity = new Vector2(Mathf.Sign(playerRB.velocity.x)*maxHorizontalSpd, playerRB.velocity.y);
         }
+        isFlying = (direction != 0 || lift != 0) && !onGround;
+        onGround = isGrounded();
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -Vector2.up, distance:distToGround, layerMask:groundLayer.value);
     }
 }
