141e8a0a1f5ac3c9ce13afef2667f4c090ab5b96
nansonzheng <nanson.zheng@mail.utoronto.ca>
2016-10-11 00:00:16 -0400

    Added layers for my scene

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -5,20 +5,32 @@ public class PlayerBehaviour : MonoBehaviour {
 
     Rigidbody2D playerRB;
     float direction, lift;
-    public float horizontalSpd = 0.6f;
-    public float verticalSpd = 0.8f;
+    public float horizontalSpd = 1.0f;
+    public float verticalSpd = 1.0f;
+    public float maxHorizontalSpd = 3.0f;
+
+    //Simulate Button spamming
+    bool verticalCD;
 
 	// Use this for initialization
 	void Start () {
         playerRB = GetComponent<Rigidbody2D>();
         direction = 0.0f;
         lift = 0.0f;
+        verticalCD = false;
     }
 	
 	// Update is used for receiving input
 	void Update () {
-            direction = Input.GetAxisRaw("Horizontal");
+        direction = Input.GetAxisRaw("Horizontal");
+        if (!verticalCD && Input.GetAxisRaw("Vertical") > 0)
+        {
             lift = Input.GetAxisRaw("Vertical");
+        }
+        else {
+            lift = 0.0f;
+        }    
+        verticalCD = Input.GetAxisRaw("Vertical") != 0.0f;
 
     }
 
@@ -26,5 +38,9 @@ public class PlayerBehaviour : MonoBehaviour {
     void FixedUpdate() {
         Vector2 playerMove = new Vector2(direction * horizontalSpd, lift * verticalSpd);
         playerRB.AddForce(playerMove, ForceMode2D.Impulse);
+        if (Mathf.Abs(playerRB.velocity.x) > maxHorizontalSpd)
+        {
+            playerRB.velocity = new Vector2(Mathf.Sign(playerRB.velocity.x)*maxHorizontalSpd, playerRB.velocity.y);
+        }
     }
 }
